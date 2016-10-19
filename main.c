04ae6577314364a28099fe7d8c52076638635b12
echevarria <quinho@gmail.com>
2016-10-19 16:01:31 -0200

    Limpando o código.

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: quinho
  *
- * Created on 18 de Outubro de 2016, 22:36
  */
 
 #include <stdio.h>
